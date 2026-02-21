6acf34456e892a9decd13c2c9f933151b9f9d70e
zoe.kang <jujuhwa@gmail.com>
2026-02-21 11:46:44 +0800

    每日任務Day 43

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import { HashRouter, NavLink, Route, Routes } from "react-router-dom";
+
+const Todo = () => {
+  return <p>這是 Todo 頁面</p>;
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
